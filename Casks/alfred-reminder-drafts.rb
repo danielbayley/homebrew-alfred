de941a784db1e1b41f9b7be033fa0ebafc791139
Daniel Bayley <daniel.bayley@me.com>
2018-05-18 17:07:44 +0100

    Add Reminder in Drafts

---
+++ Casks/alfred-reminder-drafts.rb
@@ -0,0 +1,14 @@
+cask 'alfred-reminder-drafts' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://agiletortoise.com/downloads/Reminder-in-Drafts-List.alfredworkflow'
+  name 'Reminder in Drafts List'
+  homepage 'https://agiletortoise.com/blog/2016/06/08/getting-text-into-drafts-from-a-mac'
+
+  plist = '~/Library/Preferences/com.*.Alfred-Preferences-3.plist'
+  syncfolder = File.expand_path `/usr/bin/defaults read #{plist} syncfolder`
+  workflow = "#{syncfolder.chomp}/Alfred.alfredpreferences/workflows/" + name[0]
+
+  artifact staged_path, target: workflow
+end
