d8f88c044ee411412b82af5b70334b146e647061
Daniel Bayley <daniel.bayley@me.com>
2018-08-14 19:52:24 +0100

    Revert "Remove appcasts"
    This reverts commit f9665dc2d1f956c9475e090dcfc731abff290873.

--- Casks/alfred-numi.rb
+++ Casks/alfred-numi.rb
@@ -4,6 +4,8 @@ cask 'alfred-numi' do
 
   # github.com/nikolaeu/Numi-extensions was verified as official when first introduced to the cask
   url "https://github.com/nikolaeu/Numi-extensions/releases/download/#{version}/Numi.alfredworkflow"
+  appcast 'https://github.com/nikolaeu/Numi-extensions/releases.atom',
+          checkpoint: '90ecddd1f29fec3db22ef4de0d885dd52227bacf30cdb979955c09f82365046d'
   name 'Numi'
   homepage 'http://numi.io/blog/alfred'
 
