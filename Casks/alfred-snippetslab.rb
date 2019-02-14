c020fc915129e29361aff2b69468e6a60871a671
Daniel Bayley <daniel.bayley@me.com>
2019-02-14 05:56:15 +0000

    Fix SnippetsLab cask version :latest
    The download url is not versioned.

--- Casks/alfred-snippetslab.rb
+++ Casks/alfred-snippetslab.rb
@@ -1,6 +1,6 @@
 cask 'alfred-snippetslab' do
-  version '1.7.1'
-  sha256 'df71325e941be53685895e813ed645b0cc445f156bf9d917867cb10f6633ca7e'
+  version :latest
+  sha256 :no_check
 
   # dropbox.com/s/iviryouw1da8o09 was verified as official when first introduced to the cask
   url 'https://dropbox.com/s/iviryouw1da8o09/SnippetsLabAlfred.zip?dl=1'
