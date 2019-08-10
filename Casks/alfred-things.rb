8d412173241cd9fbfe976c512ae081f9b709463e
Daniel Bayley <daniel.bayley@me.com>
2019-08-10 18:27:36 +0100

    Fix deprecated  #dots_to_slashes call
    Fixes #3

--- Casks/alfred-things.rb
+++ Casks/alfred-things.rb
@@ -2,7 +2,7 @@ cask 'alfred-things' do
   version '3,2017.05'
   sha256 '508ffea9bfb2f2ebb3e0c29b570ac9d71c5de8991aaf6bf97660e3fdff15cb98'
 
-  url "https://culturedcode.com/frozen/#{version.after_comma.dots_to_slashes}/Add-To-Do-to-Things-#{version.major}.alfredworkflow.zip"
+  url "https://culturedcode.com/frozen/#{version.after_comma.major}/#{version.after_comma.minor}/Add-To-Do-to-Things-#{version.major}.alfredworkflow.zip"
   name "Add To-Do to Things #{version.major}"
   homepage 'https://support.culturedcode.com/customer/en/portal/articles/2803574-creating-to-dos-with-a-launcher'
 
