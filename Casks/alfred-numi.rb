c801be961a67e8f6babbaef1cb8b3ef4a919eb11
Daniel Bayley <daniel.bayley@me.com>
2019-12-10 01:37:01 +0000

    Update Numi to 3.20.4
    Fixes upstream release restructure.

--- Casks/alfred-numi.rb
+++ Casks/alfred-numi.rb
@@ -1,10 +1,10 @@
 cask 'alfred-numi' do
-  version '1.3'
+  version '3.20.4'
   sha256 'de88c986acdfc418263f1e176aabe6dc8c251f4adc9a7eff974120d2e3fd5b02'
 
-  # github.com/nikolaeu/Numi-extensions was verified as official when first introduced to the cask
-  url "https://github.com/nikolaeu/Numi-extensions/releases/download/#{version}/Numi.alfredworkflow"
-  appcast 'https://github.com/nikolaeu/Numi-extensions/releases.atom'
+  # github.com/nikolaeu/Numi was verified as official when first introduced to the cask
+  url "https://github.com/nikolaeu/Numi/releases/download/mac-#{version}/Numi.alfredworkflow"
+  appcast 'https://github.com/nikolaeu/Numi/releases.atom'
   name 'Numi'
   homepage 'https://numi.io/docs/#alfred'
 
