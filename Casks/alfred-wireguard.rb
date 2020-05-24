a70762708a0a2c68ede4bf4cc67354b5e663e742
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-24 16:22:07 +0000

    Add appcast to alfred-wiregard

--- Casks/alfred-wireguard.rb
+++ Casks/alfred-wireguard.rb
@@ -3,6 +3,7 @@ cask 'alfred-wireguard' do
   sha256 '9118082580961af6bb1bb4c6c3a53ba4551e99683d7f497789f717d4ef3566cd'
 
   url "https://github.com/Chrede88/alfred-wireguard/releases/download/v#{version}/alfred-wireguard.alfredworkflow"
+  appcast 'https://github.com/chrede88/alfred-wireguard/releases'
   name 'alfred-wireguard'
   homepage 'https://github.com/Chrede88/alfred-wireguard'
 
